d6c1b40ecb5fdb1454d5ed71aa6b2d23cec95193
david <makodavid1996@gmail.com>
2023-03-15 14:20:47 +0100

    add spring initializr maven project

---
+++ src/main/java/com/main/tracker/ExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.main.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.main.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
