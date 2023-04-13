a7951e5cb968dc0e52489cf8791e55aca13c7c17
david <makodavid1996@gmail.com>
2023-04-13 22:24:46 +0200

    add jwtService and jwtAuthFilter WIP

--- backend/src/main/java/com/main/tracker/configuration/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/main/tracker/configuration/JwtAuthenticationFilter.java
@@ -1,6 +1,43 @@
 package com.main.tracker.configuration;
 
+import com.main.tracker.configuration.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JwtAuthenticationFilter {
+import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final int BEGIN_INDEX = 7;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        String jwt;
+        String userName;
+
+        if(header == null || !header.startsWith("Bearer ") ){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        jwt = header.substring(BEGIN_INDEX);
+        userName = jwtService.extractUserName(jwt);
+
+        //TODO
+
+    }
 }
