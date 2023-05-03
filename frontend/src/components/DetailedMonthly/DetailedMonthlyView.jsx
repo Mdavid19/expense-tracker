5a743f0cf91bc9f582897b9f66665ca622872819
david <makodavid1996@gmail.com>
2023-05-03 18:39:55 +0200

    add detailed monthly expense view

---
+++ frontend/src/components/DetailedMonthly/DetailedMonthlyView.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './detaildView.css'
+function DetailedMonthlyView(props) {
+    return (
+        <div className={'detailed'}>
+            <div className="traveling">
+
+            </div>
+            <div className="food"></div>
+            <div className="gift"></div>
+            <div className="permanent"></div>
+            <div className="freetime"></div>
+            <div className="healthy"></div>
+        </div>
+    );
+}
+
+export default DetailedMonthlyView;
