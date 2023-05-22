981175d50cfa71148af897df1dc8a05fb9f66d33
david <makodavid1996@gmail.com>
2023-05-22 14:19:24 +0200

    add type selector to add expense dialog

--- frontend/src/components/AddExpenseDialog/AddExpenseDialog.jsx
+++ frontend/src/components/AddExpenseDialog/AddExpenseDialog.jsx
@@ -6,6 +6,11 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogActions from '@mui/material/DialogActions'
 import {createTheme, ThemeProvider} from "@mui/material";
 import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
 import {useUser} from "../../context/UserProvider";
 
 const theme = createTheme({
@@ -24,6 +29,7 @@ function AddExpenseDialog({date}) {
     const [open, setOpen] = React.useState(false);
     const [amount, setAmount] = React.useState(null); // kitenni a mainre és csak a frontenden hozzáadni a useSATEHEZ ÉS NEM KELL FETCH CHAK HA TAB VÁLTÁS VAN
     const {user} = useUser();
+    const [type, setType] = React.useState("FOOD");
     const [localDate, setLocalDate] = React.useState(date);
 
     React.useEffect(()=>{
@@ -43,7 +49,8 @@ function AddExpenseDialog({date}) {
     const data = {
         username:user.username,
         value:amount,
-        date: localDate
+        date: localDate,
+        receiptType:type
     }
 
     function sendData(){
@@ -65,19 +72,40 @@ function AddExpenseDialog({date}) {
 
     }
 
+    const handleChange = (event) => {
+        setType(event.target.value)
+    }
+
     return (
         <ThemeProvider theme={theme}>
-            <div>
+            <div >
                 <Button color="inherit" variant={"outlined"} onClick={handleOpen}>Add Expense</Button>
                 <Dialog open={open} onClose={handleClose}>
                     <DialogTitle>ADD YOUR EXPENSE</DialogTitle>
-                    <DialogContent>
+                    <DialogContent sx={{display:"flex", flexDirection:"column", justifyContent:"space-between", minHeight:"150px",maxHeight:"200px"}}>
                         <TextField
                         aria-label={'Amount'}
                         name={'Amount'}
                         placeholder={'Amount'}
                         variant={"filled"}
                         fullWidth onChange={(e)=>{setAmount(parseInt(e.target.value))}}/>
+                        <Box sx={{ minWidth: 120}}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={type}
+                                    label="Type"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={"FOOD"}>Food</MenuItem>
+                                    <MenuItem value={"ENTERTAINMENT"}>Entertainment</MenuItem>
+                                    <MenuItem value={"HEALTH"}>Health</MenuItem>
+                                    <MenuItem value={"TRAVELLING"}>Travelling</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleClose} color="primary">
